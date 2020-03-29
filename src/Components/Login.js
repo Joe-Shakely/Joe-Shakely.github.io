0bcdd2bbd60a921616405904cfb1ade0aadf9daf
messidagod <joeshakely@gmail.com>
2020-03-28 22:54:17 -0700

    removed unnecessary

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -11,10 +11,6 @@ export default class Login extends Component {
                         <div id="innerwrapper">
                             <div id="logincontainer" className="directional">
                                 <div className="container_lft">
-                                    <div id="webserverID" runat="server"></div>
-                                    <div id="clientlogoid" className="client-logo" runat="server"></div>
-                                    <div id="logintextid" className="logintext" runat="server">
-                                    </div>
                                 </div>
                                 <div className="columnwidth46">
                                     <div className="client-logo">
@@ -24,21 +20,17 @@ export default class Login extends Component {
                                         </ul>
                                         <ul className="divcenter" id="tblLogin" runat="server">
                                             <li>
-                                                <input type="text" id="Username" name="Username" placeholder="UserName" onfocus="onFocus(this,'UserName');"
-                                                    onblur="onBlur(this,'UserName');" />
+                                                <input type="text" id="Username" name="Username" placeholder="UserName" />
                                             </li>
                                             <li>
-                                                <input type="text" id="Password_Text" placeholder="Password" onfocus="onFocus(this,'Password','Password');" />
-                                                <input type="password" id="Password" name="Password" onblur="onBlur(this,'Password','Password_Text');"
-                                                    style={{display: "none"}} />
+                                                <input type="text" id="Password_Text" placeholder="Password" />
+                                                <input type="password" id="Password" name="Password" style={{ display: "none" }} />
                                             </li>
                                             <li>
-                                                <input type="text" id="Server" name="Server" placeholder="Server" onfocus="onFocus(this,'Server');"
-                                                    onblur="onBlur(this,'Server');" />
+                                                <input type="text" id="Server" name="Server" placeholder="Server" />
                                             </li>
                                             <li>
-                                                <input type="text" id="Database" name="Database" placeholder="Database Name" onfocus="onFocus(this,'Database Name');"
-                                                    onblur="onBlur(this,'Database Name');" />
+                                                <input type="text" id="Database" name="Database" placeholder="Database Name" />
                                             </li>
                                             <li><span className="arrow">&nbsp;</span>
                                                 <input type="submit" id="cmdLogin" name="cmdLogin" className="loginformbutton" value="LOGIN"
@@ -47,18 +39,11 @@ export default class Login extends Component {
                                         </ul>
                                     </div>
                                 </div>
-                                <div style={{textAlign: "center"}} id="Div1">
-                                </div>
                             </div>
-                            <div className="loginshadow">
-                                &nbsp;
-                                </div>
                         </div>
                     </div>
                 </div>
-
             </form>
-
         );
     }
 }
