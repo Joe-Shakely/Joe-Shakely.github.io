55726a480053abf07a77d2783240595a638958fb
messidagod <joeshakely@gmail.com>
2020-03-28 22:26:22 -0700

    nothing

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,6 @@ class About extends Component {
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
       var phone = this.props.data.phone;
-      var email = this.props.data.email;
     }
 
     return (
