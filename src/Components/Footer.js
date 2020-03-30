6801e7c98e8d26be9cc81c223d4b9a81ed7a62ee
messidagod <joeshakely@gmail.com>
2020-03-29 17:35:10 -0700

    add new files

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
         return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
     }
@@ -12,29 +12,29 @@ class Footer extends Component {
     return (
       <footer>
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
               {networks}
-           </ul>
+            </ul>
 
-           <ul className="copyright">
-           <li>
+            <ul className="copyright">
+              <li>
                 &copy;&nbsp;
                 <span>
                   Joe Shakely&nbsp;
                   <span>
-                  {(new Date().getFullYear())}
+                    {(new Date().getFullYear())}
                   </span>
                 </span>
               </li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
+            </ul>
 
+          </div>
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
