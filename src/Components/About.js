5cb590011a9fb4ee7bfbbbd2bb5392a7a66eba26
messidagod <joeshakely@gmail.com>
2021-07-19 12:09:25 -0700

    d

--- src/Components/About.js
+++ src/Components/About.js
@@ -65,9 +65,9 @@ class About extends Component {
                 height="500"
                 src="https://www.youtube.com/embed/IRY35xYab0s?start=26"
                 title="YouTube video player"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
+                allowFullScreen
               ></iframe>
             </div>
           </div>
